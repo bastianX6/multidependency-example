e1a092c8c1d8ab5bd65c629049724435f3fe0e02
Bastian Veliz Vega <bastianx6@gmail.com>
2022-08-07 17:15:28 -0400

    Initial Commit

---
+++ ItunesExample/ItunesExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItunesExampleApp.swift
+//  ItunesExample
+//
+//  Created by Bastián Véliz Vega on 07-08-22.
+//
+
+import SwiftUI
+
+@main
+struct ItunesExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
